9e6fec60522e499371ac0ab2859d8d6bc8861f0b
jinho1011 <gino9940@gmail.com>
2022-10-04 13:16:40 +0900

    Add extension test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,38 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import scanner from '../../scanner';
+
+const document = `
+	import styled from '@emotion/styled'; // ImportDeclaration
+
+	const BigText = 123 // VariableDeclaration
+	let var2 = "asfc" // VariableDeclaration
+	export var var3 = {} // ExportNamedDeclaration
+	const func2 = () => console.log(1) // VariableDeclaration
+	function func1() {} // FunctionDeclaration
+	const func3 = () => {
+		const varInFunc1 = 123;
+		const funcInFunc = () => {}
+	} // VariableDeclaration
+
+	export {func1, func2} // ExportNamedDeclaration
+	export default BigText; // ExportDefaultDeclaration
+`;
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('@babel/parser test', () => {
+		vscode.window.createInputBox();
+		const nodes = scanner(document);
+		console.log('🚀 ~ file: extension.test.ts ~ line 23 ~ test ~ nodes', nodes);
+
+
+
+		// vscode.window.showInformationMessage(nodes);
+
+
+		// assert.strictEqual(1, [1, 2, 3].indexOf(0));
 	});
 });
+
