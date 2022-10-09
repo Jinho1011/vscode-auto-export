e1360843e321581b33a09196f713726d5e184b16
jinho1011 <gino9940@gmail.com>
2022-10-10 01:14:14 +0900

    Implement auto-export-js.exportAllAsNamed

--- src/extension.ts
+++ src/extension.ts
@@ -10,64 +10,61 @@ import Parser from './parser';
 
 export function activate(context: ExtensionContext) {
   context.subscriptions.push(
-    commands.registerCommand('auto-export-js.exportAll', () => {})
-  );
-
-  context.subscriptions.push(
-    commands.registerCommand('auto-export-js.test', () => {
+    commands.registerCommand('auto-export-js.exportAllAsNamed', () => {
       const editor: TextEditor | undefined = window.activeTextEditor;
 
       if (editor) {
         const document = editor.document;
         const textDocument = document.getText();
-        const invalidRange = new Range(
-          0,
-          0,
-          document.lineCount /*intentionally missing the '-1' */,
-          0
+        const documentRange = document.validateRange(
+          new Range(0, 0, document.lineCount, 0)
         );
-        const fullRange = document.validateRange(invalidRange);
 
-        const parser: Parser = new Parser(textDocument);
-        const declarationStatements = parser.getExportableStatements();
+        try {
+          const parser: Parser = new Parser(textDocument);
+          const exportableStatements = parser.getExportableStatements();
 
-        if (declarationStatements.length) {
-          const names = parser.getVariablesName(declarationStatements);
-          const exportStatement = parser.getNamedExportStatement(names);
-          const existedNamedExports = parser.getExportNamedDeclarations();
+          if (exportableStatements.length) {
+            const existedNamedExports = parser.getExportNamedDeclarations();
+            const exportStatement =
+              parser.getNamedExportStatement(exportableStatements);
 
-          editor.edit((editBuilder) => {
-            if (existedNamedExports.length) {
-              existedNamedExports.map((existedNamedExport, i) => {
-                const startPosition = new Position(
-                  existedNamedExport.loc!.start.line - 1,
-                  existedNamedExport.loc!.start.column
-                );
-                const endPosition = new Position(
-                  existedNamedExport.loc!.end.line,
-                  0
-                );
-                endPosition.isAfter(new Position(0, 0));
-                console.log(existedNamedExport.loc);
-                console.log(startPosition, endPosition);
-                editBuilder.delete(new Range(startPosition, endPosition));
-              });
-            }
-
-            editBuilder.insert(
-              new Position(fullRange.end.line, fullRange.end.character),
-              '\n'
-            );
-            editBuilder.insert(
-              new Position(fullRange.end.line + 1, 0),
-              exportStatement
-            );
-          });
-        } else {
-          window.showInformationMessage('Cannot find exportable variables.');
+            editor.edit((editBuilder) => {
+              /**
+               * prevent redeclare exported variables
+               */
+              if (existedNamedExports.length) {
+                existedNamedExports.map((existedNamedExport, i) => {
+                  const startPosition = new Position(
+                    existedNamedExport.loc!.start.line - 1,
+                    existedNamedExport.loc!.start.column
+                  );
+                  const endPosition = new Position(
+                    existedNamedExport.loc!.end.line,
+                    0
+                  );
+                  editBuilder.delete(new Range(startPosition, endPosition));
+                });
+              }
+              /**
+               * inserts export statement at the end of the document
+               */
+              editBuilder.insert(
+                new Position(
+                  documentRange.end.line,
+                  documentRange.end.character
+                ),
+                '\n' + exportStatement
+              );
+            });
+          } else {
+            window.showInformationMessage('Cannot find exportable statement.');
+          }
+        } catch (e) {
+          window.showInformationMessage('Cannot parse current document.');
         }
       } else {
-        window.showInformationMessage('Cannot find and document');
+        window.showInformationMessage('Cannot find and document.');
       }
     })
   );
