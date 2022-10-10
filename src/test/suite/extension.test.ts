38034710f48c341fa0e7a651165da2ec1ffc67d2
jinho1011 <gino9940@gmail.com>
2022-10-10 11:57:19 +0900

    Remove test

--- src/test/suite/extension.test.ts
+++
@@ -1,8 +0,0 @@
-import * as assert from 'assert';
-import * as vscode from 'vscode';
-
-suite('Extension Test Suite', () => {
-  vscode.window.showInformationMessage('Start all tests.');
-
-  test('@babel/parser test', () => {});
-});
